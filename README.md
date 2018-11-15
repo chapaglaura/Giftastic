18a99379234f5f01095a8893478bce37f39976de
LACH <laurachg8@gmail.com>
2018-11-15 16:12:54 -0600

    Added Bonus

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Giftastic
+# **GIFstatic**
+
+## **What is it?**
+A GIF search with none other than human’s best friend (though you can add any other categories). You can choose between a large variety of dog breeds or, if you prefer, add any other one by filling the text box and submitting a new one. As simple as that. Have fun!
+
+## **How to play:**
+1. Select a dog breed from the buttons or add your own (this will create a new button).
+
+2. Click the dog breed you want to look for GIFs of.
+
+3. 10 GIFs of this selection will be added to the page but just their main frame.
+
+4. If you would like to see the animated GIF, just click on it (as easy as pie!).
+
+5. A second click on it will pause the GIF.
+
+6. You can continue choosing different dog breeds or adding new ones, the GIFs will keep being added at the end of the page.
+
+**Hope you like it!**
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+//all initial dog breeds array
 var topics = [
     "labrador",
     "schnauzer",
@@ -20,87 +21,180 @@ var topics = [
     "dalmatian"
 ];
 
-var btn;
-var imageURL = [];
-var stillImageURL = [];
-var url;
+var pa = [];
 
 
 $(document).ready(function () {
 
+    //loop for creating all buttons from the topics array
     for (var i = 0; i < topics.length; i++) {
 
-        createButton(topics[i]);
+        createButton(topics[i]); //function to create buttons of all topics
+
     }
 
+    //submit button click event handler
     $(".add-submit").click(function () {
 
-        var added = $(".add-input").val();
+        var added = $(".add-input").val(); //stores the textbox value in variable
+        topics.push(added); //adds textbox value to topics array
 
-        createButton(added);
-    })
+        createButton(added); //function to create button with new topic
+    });
 
+    //topics buttons click event handler with delegation
     $(".topics-buttons").on('click', '.option', function () {
-        var search = $(this).text();
-        search.split(' ').join('+');
+
+        var search = $(this).text(); //stores text of button clicked in variable
+
+        search.split(' ').join('+'); //replaces spaces with plus signs
+
+        //API url for call concatenating variable and specifying key and limit
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=AefenpNDGmxeIlw72Y8YH5XkhUgP1kSu&limit=10";
+
+        //API call
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
 
-            var r = response;
+            //loop for retrieving limited amount of data
+            for (var i = 0; i < response.data.length; i++) {
 
-            for (var i = 0; i < r.data.length; i++) {
-                var chosen = r.data[i];
-                var img = $("<img class='gif image-fluid'>");
-                var rtg = $("<h3>");
-                var title = $("<h4>");
-                var score = $("<h4>");
-                var dwn = $("<a download class='gif-link'>");
-                var dwnBtn = $("<button class='btn btn-info'>");
-                dwn.append(dwnBtn);
-                rtg.text("Rating: " + chosen.rating);
-                img.attr('src', chosen.images.original_still.url);
-                title.text("Title: " + chosen.title);
-                score.text("Score: " + chosen._score);
-                url = chosen.images.original.url;
+                var chosen = response.data[i]; //stores current data in variable
 
-                dwn.text("Download!");
+                var rtg = $("<h3>"); //creates an h3 element
+                var img = $("<img class='gif image-fluid'>"); //creates an img element
+                var title = $("<h4>"); //creates an h4 element
+                var score = $("<h4>"); //creates an h4 element
+                var dwn = $("<a download class='gif-link'>"); //creates an a element
+                var dwnBtn = $("<button class='btn btn-info'>"); //creates an button element
+                var fav = $("<i class='far fa-star star'></i>");
 
-                var div = $("<div>");
-                div.append(rtg, img, title, score, dwn);
-                $(".gifs").append(div);
+                rtg.text("Rating: " + chosen.rating); //sets text in h3 to rating property
+                img.attr('src', chosen.images.original_still.url); //sets source attribute in img to GIF's still image
+                title.text("Title: " + chosen.title); //sets text in h4 to title property
+                score.text("Score: " + chosen._score); //sets text in h4 to score property
+
+                //sets data attributes of img to control GIF state
+                img.attr('data-still', chosen.images.original_still.url);
+                img.attr('data-animated', chosen.images.original.url);
+                img.attr('data-state', 'still');
+
+                dwn.attr('href', img.attr('data-animated')); //sets link to GIF url for download
+                dwn.append(dwnBtn); //appends button to a element
+                dwn.text("Download!"); //sets text in a element to download
+
+                var div = $("<div>"); //creates a div element
+                div.append(rtg, img, title, score, dwn, fav); //appends previous elements to div
+                $(".gifs").append(div); //appends div to div with all GIFs
+            }
+        });
+
+        queryURL = "http://www.omdbapi.com/?t=" + search + "&apikey=trilogy&plot=full";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            var h3 = $("<h3>");
+            var title = $("<p>");
+            var year = $("<p>");
+            var actors = $("<p>");
+            var plot = $("<p>");
+            var poster = $("<img class='image-fluid'>")
+
+            console.log(response.Response);
+            if (response.Response === "True") {
+                h3.text("Movie:");
+            }
+            else {
+                h3.text(response.Error)
             }
 
-            $(".container").on('click', '.gif', function () {
+            title.text(response.Title);
+            year.text(response.Year);
+            actors.text(response.Actors);
+            plot.text(response.Plot);
 
-                url = $(this).attr('src');
+            if (response.Poster !== "N/A") {
+                poster.attr('src', response.Poster);
+                $(".movies").html(h3).append(title, year, actors, plot, poster);
+            }
+            else {
+                $(".movies").html(h3).append(title, year, actors, plot);
+            }
 
-                var index = url.indexOf("giphy_s");
-
-                if (index !== -1) {
-                    url = url.replace("giphy_s", "giphy");
-                    $(this).attr('src', url);
-                }
-                else {
-                    url = url.replace("giphy.gif", "giphy_s.gif");
-                    $(this).attr('src', url);
-                }
-
-
-
-            });
-
-
-
-
-        });
+        })
     });
+
+    //GIFs click event handler with delegation
+    $(".gifs, .favs").on('click', '.gif', function () {
+
+        var state = this.dataset.state; //stores the data-state attribute value in variable
+
+        //checks if GIF state is still or animated
+        if (state === "still") {
+            $(this).attr('src', this.dataset.animated); //changes GIF source to animated GIF
+            this.dataset.state = 'animated'; //sets data-state attribute to animated
+        }
+        else if (state === "animated") {
+            $(this).attr('src', this.dataset.still); //changes GIF source to still GIF
+            this.dataset.state = 'still'; //sets data-state attribute to still
+        }
+    });
+
+    $(".gifs, .favs").on('mouseenter', '.star', function () {
+        if ($(this).hasClass('far')) {
+            $(this).removeClass('far').addClass('fas');
+        }
+        else {
+            $(this).removeClass('fas').addClass('far');
+        }
+    }).on('mouseleave', '.star', function () {
+        if ($(this).hasClass('far')) {
+            $(this).removeClass('far').addClass('fas');
+        }
+        else {
+            $(this).removeClass('fas').addClass('far');
+        }
+    });
+
+    $(".gifs").on('click', '.star', function () {
+        var gfav = $(this).parent();
+        $('.favs').append(gfav);
+
+        pa = $('.favs').html();
+
+        $(this).removeClass('fas').addClass('far');
+
+        sendToStorage();
+    });
+
+    $(".favs").on('click', '.star', function () {
+        var gfav = $(this).parent();
+        $('.gifs').append(gfav);
+
+        pa = $('.favs').html();
+
+        $(this).removeClass('far').addClass('fas');
+
+        sendToStorage();
+    });
+
+
+    if (localStorage.getItem('favorites') !== null) {
+        var st = localStorage.getItem('favorites');
+        pa = JSON.parse(st);
+
+        $('.favs').html(pa);
+    }
 
 });
 
-
+//function to create buttons
 function createButton(b) {
 
     var btn = $("<button>");
@@ -110,4 +204,10 @@ function createButton(b) {
 
     $(".topics-buttons").append(btn);
 
+}
+
+function sendToStorage() {
+    localStorage.clear();
+
+    localStorage.setItem('favorites', JSON.stringify(pa));
 }
